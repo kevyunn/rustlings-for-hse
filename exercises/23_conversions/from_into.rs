5a5f2788a529d10fc1e0ac5c1617fd24b1808a47
s_kevyun <sangadzhiev1406@mail.ru>
2026-02-28 18:21:25 +0300

    ex 22-23 + quizzes

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -34,7 +34,24 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let parts = s.split(',').collect::<Vec<&str>>();
+        if parts.len() != 2 {
+            return Person::default();
+        }
+
+        let name = parts[0].to_string();
+        if name.is_empty() {
+            return Person::default();
+        }
+
+        let age = parts[1].parse::<u8>();
+        if let Err(_e) = age {
+            return Person::default();
+        }
+        
+        Person { name: name, age: age.unwrap() }
+    }
 }
 
 fn main() {
